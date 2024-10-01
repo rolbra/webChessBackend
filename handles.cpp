da27228bc4cea19ea2f402b2543489d92789dd13
rolbra <roli_brandmeister@web.de>
2024-10-01 21:00:02 +0200

    extend api

--- handles.cpp
+++ handles.cpp
@@ -5,6 +5,13 @@
 using namespace web;
 using namespace web::http;
 
+void setResponseHeaders( http_response &response )
+{
+    response.headers().add(U("Access-Control-Allow-Methods"), U("POST, OPTIONS"));
+    response.headers().add(U("Access-Control-Allow-Headers"), U("Content-Type, Access-Control-Allow-Origin"));
+    response.headers().add(U("Access-Control-Allow-Origin"), U("*"));
+}
+
 void handle_options( http_request request )
 {
     std::cout << "incoming OPTIONS request\n";
@@ -12,9 +19,7 @@ void handle_options( http_request request )
     std::cout << request.to_string() << "\n";
     
     http_response response(status_codes::OK);
-    response.headers().add(U("Access-Control-Allow-Methods"), U("POST, OPTIONS"));
-    response.headers().add(U("Access-Control-Allow-Headers"), U("Content-Type, Access-Control-Allow-Origin"));
-    response.headers().add(U("Access-Control-Allow-Origin"), U("*"));
+    setResponseHeaders( response );
 
     request.reply( response );
 }
@@ -192,39 +197,59 @@ void handle_post( http_request request )
     std::cout << "-------------------------\n";
     std::cout << request.to_string() << "\n";
     
-    request.extract_json()
-    .then( [=]( json::value jsonData )
-    {
-        json::value positions;
-        initPositions(positions);
+    http_response response(status_codes::OK);
+    setResponseHeaders( response );
 
+    auto paths = uri::split_path(uri::decode(request.relative_uri().path()));
+    if( paths.empty() || paths.size() > 1 )
+    {
+        json::value responseData;
+        responseData["help"] = json::value::string("supported services: api/positions");
+        response.set_body(responseData);
+        request.reply(response);
+        return;
+    }
+    if( paths[0] == "positions" )
+    {
+        request.extract_json()
+        .then( [=]( json::value jsonData )
+        {
+            json::value positions;
+            initPositions(positions);
+
+            http_response response(status_codes::OK);
+            setResponseHeaders(response);
+            response.set_body(positions);
+
+            request.reply(response);
+        })
+        .then( [=]( pplx::task<void> previousTask )
+        {
+            try
+            {
+                previousTask.get(); // This will throw if there was an error in the previous task
+            }
+            catch (const std::exception& e)
+            {
+                std::cerr << "Error extracting JSON: " << e.what() << "\n";
+                
+                json::value responseData;
+                responseData["message"] = json::value::string("Invalid JSON format");
+                request.reply(status_codes::OK, responseData);
+            }
+        })
+        .wait();
+    }
+    if( paths[0] == "gameStatus" )
+    {
         http_response response(status_codes::OK);
-        response.headers().add(U("Access-Control-Allow-Methods"), U("POST, OPTIONS"));
-        response.headers().add(U("Access-Control-Allow-Headers"), U("Content-Type, Access-Control-Allow-Origin"));
-        response.headers().add(U("Access-Control-Allow-Origin"), U("*"));
-        response.set_body(positions);
+        setResponseHeaders(response);
+        
+        json::value responseData;
+        responseData["Matchup"] = json::value::string("Benni vs. Roli");
+        responseData["Game created"] = json::value::string("Oct/01/2024");
+        response.set_body(responseData);
 
         request.reply(response);
-    })
-    .then( [=]( pplx::task<void> previousTask )
-    {
-        try
-        {
-            previousTask.get(); // This will throw if there was an error in the previous task
-        }
-        catch (const std::exception& e)
-        {
-            std::cerr << "Error extracting JSON: " << e.what() << "\n";
-            
-            json::value responseData;
-            responseData["message"] = json::value::string("Invalid JSON format");
-            request.reply(status_codes::OK, responseData);
-        }
-    })
-    .wait();
-    
-    json::value responseData;
-    responseData["message"] = json::value::string("What's up?");
-
-    request.reply( status_codes::OK, responseData );
+    }
 }
